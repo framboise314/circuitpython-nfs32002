2d07cfbc052d0a04cb78ffc3fe2358dc90244545
samuel b <samuel.braikeh@yahoo.fr>
2023-05-26 21:06:14 +0200

    Update example.py

--- example.py
+++ example.py
@@ -9,3 +9,4 @@ radio = CC1101_NFS32002(board.GP6, board.GP7, board.GP4, board.GP5, board.GP8)
 while True:
     message_recu = radio.wait_for_data()    
     print("Ouistici !")
+    time.sleep(0.1)
